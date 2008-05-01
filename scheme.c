ae8a0763c83d15a7418fa9e252ffe9ab97d9c99d
Kyle Cronin <kyle@Valencia.local>
2008-05-01 02:01:48 -0400

    parse now called "read" to conform to the R in REPL

--- scheme.c
+++ scheme.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-**********************************************************
-
-well it's been several days since I last worked on this stuff. in that time I have learned some more interesting things about C structs - enough to make me want to try this again. basically, if you have a pointer to a struct, you can convert that pointer seamlessly to one to the first item in the struct. this has the advantage of allowing an object-like hierarchy.
-
-each type will have its own typedef. the atom typedef will be the supertype that will be used to extend all the other types.
-
-Since the only data I want to store is the type of object it really is I think I will hybridize the type enum and the atom struct.
-
-**********************************************************
-*/
-
 #include "types.c"
-
 
 
 char **cw (char **p)
@@ -26,36 +13,7 @@ char **cw (char **p)
 void ip (char **p) { (*p)++; }
 void dp (char **p) { (*p)--; }
 
-/**
-	I finally know what my problem is - I'm trying to read the close
-	paren off a list that I've created out of thin air.
-	
-	This practice of modifying the list is dangerous, but there might be a
-	way to salvage this
-	
-	If I insert a paren, I don't expect one at the end.
-	
-	
-	
-	new problem: either the read or the print of (1 (2) 3) is (1 (2))
-	
-	
-	just tested the print, it works fine. so it is, as I suspected, the read
-	operation.
-	
-	we need to step through this:
-	parse (1 (2) 3)
-		read (
-		parse 1
-		parse ((2) 3)
-			read (
-			parse (2)
-			
-			
-	I, uh, fixed it. just needed an ip(input);
-**/
-
-atom *parse (char **input)
+atom *read (char **input)
 {	
 	if (**cw(input) == '(')
 	{
@@ -67,7 +25,7 @@ atom *parse (char **input)
 			return NULL;
 		}
 		
-		atom *car = parse(input), *cdr;
+		atom *car = read(input), *cdr;
 		
 		if (**cw(input) == ')')
 		{
@@ -80,54 +38,29 @@ atom *parse (char **input)
 			if (**input == '.')
 			{
 				ip(input);
-				cdr = parse(input);
+				cdr = read(input);
 				if (**cw(input) == ')')
 					ip(input);
 				else
-					printf("parse error: invalid dotted pair\n");
+					printf("read error: invalid dotted pair\n");
 			}
 			else
 			{
 				dp(input);
 				**input = '(';
-				cdr = parse(input);
-				/*printf("checking for ) at %i\n", *input - start);
-				if (**cw(input) == ')')
-					ip(input);
-				else
-					printf("parse error: invalid list [ read '%i' ]\n", *input - start);*/
+				cdr = read(input);
 			}	
 		}	
 		return newcons(car, cdr);
 	}
 	else
 	{
-		//ret->t = integer;
 		atom *ret = newint(atoi(*input));
 		while (**input >= 48 && **input <= 57)
 			ip(input);
 		return ret;
 	}
 }
-
-/*
-
-OK, we're sort of in a pickle here because of the list. The
-easiest thing to do is to print it recursively, but this would
-result in unwanted parenthesis. I guess the best way to do this
-would be to create a loop instead
-
-- if atom, print atom
-- if list, print open paren '(' and enter list printing loop:
-	- print car
-	- is cdr null?
-		- yes: end loop
-		- no: is cdr cons?
-			- no: print dot '.', print cdr, end loop
-			- yes: set head to cdr, continue loop
-- print close paren ')'
-
-*/
 
 void print (atom *x)
 {
@@ -163,9 +96,6 @@ void print (atom *x)
 	}
 }
 
-
-
-
 int main (int argc, const char * argv[])
 {
 	char buff[256];
@@ -176,10 +106,9 @@ int main (int argc, const char * argv[])
 		printf("> ");
 		gets(buff);
 		p = buff;
-		print(parse(&p));
+		print(read(&p));
 		printf("\n");
 	}
 	
     return 0;
 }
-
