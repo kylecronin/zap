51a0178265ad90c8559117f6563867cc1bdbe9d4
Kyle Cronin <kyle@Valencia.local>
2008-05-02 22:21:44 -0400

    lambda works! I'm on a roll!

--- axioms.c
+++ axioms.c
@@ -3,7 +3,7 @@ atom *add(atom *args, nspace *n) {
 		return newint(0);
 	if (*args != tcons)
 	{
-		printf("add: argument list makes no sense!\n");
+		printf("+: argument list makes no sense!\n");
 		return NULL;
 	}
 	
@@ -15,4 +15,42 @@ atom *add(atom *args, nspace *n) {
 		return newint(car->i + cdr->i);
 	else
 		return NULL;
+}
+
+atom *quit(atom *args, nspace *n) {
+	exit(0);
+}
+
+atom *mult(atom *args, nspace *n) {
+	if (!args)
+		return newint(1);
+	if (*args != tcons)
+	{
+		printf("*: argument list makes no sense!\n");
+		return NULL;
+	}
+	
+	acons *al = ccons(args);
+	aint *car = cint(eval(al->car, n));
+	aint *cdr = cint(mult(al->cdr, n));
+	
+	if (car && cdr)
+		return newint(car->i * cdr->i);
+	else
+		return NULL;
+}
+
+atom *lambda(atom *args, nspace *n) {
+	if (!args || *args != tcons)
+	{
+		printf("lambda: bad syntax\n");
+		return NULL;
+	}
+	acons *c = ccons(args);
+	if (!c->car || *(c->car) == tsym || *(c->car) == tcons)
+		if (c->cdr && *(c->cdr) == tcons)
+			return newfun(c->car, ccons(c->cdr)->car, n);		
+
+	printf("lambda: bad syntax\n");
+	return NULL;
 }
